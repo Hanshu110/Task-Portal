34854cfa4f67e434b9de43de0a85d4815c4db9bc
Hanish <120237670+Hanshu110@users.noreply.github.com>
2025-03-04 14:18:16 +0530

    Update adminFormvalidation.php

--- files/adminFormvalidation.php
+++ files/adminFormvalidation.php
@@ -9,10 +9,10 @@ if (isset($_POST['UN']) && isset($_POST['PASS'])) {
 }
 $q = mysqli_query(mysqli_connect("localhost", "root", "", "ttms"), "SELECT name FROM admin WHERE name = '$id' and password = '$password' ");
 if (mysqli_num_rows($q) == 1) {
-    echo 'welcome admin';
+    header("Location:addteachers.php");
 } else {
     $message = "Username and/or Password incorrect.\\nTry again.";
     echo "<script type='text/javascript'>alert('$message');</script>";
-}
 
+}
 ?>
